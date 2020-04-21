aa3b0f5ee0a80604a5ef81e7120e7913f563a922
gcmartin <gscalica@gmail.com>
2020-04-20 17:42:14 -0700

    Add code for POST request to store new note

--- Develop/server.js
+++ Develop/server.js
@@ -31,14 +31,58 @@ app.get("/notes", function(req, res){
 // API routes
 // (GET) API Notes route
 app.get("/api/notes", function(req, res){
-    res.sendFile(__dirname + "/db/db.json");
+    // read the db.json file
+    fs.readFile("./db/db.json", "utf8", function(err, data){
+        if(err){
+            console.log(err);
+        }
+            // parse the data and store to variable
+            let noteData = JSON.parse(data);
+
+            // respond to user with the json data
+            res.json(noteData);
+    });
+
+    
+    // res.sendFile(__dirname + "/db/db.json");
 });
 
 // // (POST) API Notes route
 app.post("/api/notes", function(req, res){
-    // save the new note to a variable
-    // push the new note to db.json
-    // respond to client with the new note
+    // read the db.json file
+    fs.readFile("./db/db.json", "utf8", function(err, data){
+        if(err){
+            console.log(err);
+        }
+            // parse the data and store to variable
+            let noteData = JSON.parse(data);
+
+            // store the newNote from the POST request to a new variable
+            let newNote = req.body;
+            console.log(newNote);
+
+            // push the newNote into noteData (it's an array)
+            noteData.push(newNote); 
+
+            // see what the data is
+            console.log(JSON.stringify(noteData));
+
+            // write file
+            fs.writeFile("./db/db.json", JSON.stringify(noteData), function(err){
+                if(err){
+                    return console.log(err);
+                }
+                    console.log("success!");
+            })
+
+            // respond to the user with the new note
+            res.json(noteData);
+
+        
+    });
+    // X save the new note to a variable
+    // X push the new note to db.json
+    // X respond to client with the new note
 
 });
 
