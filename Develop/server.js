0f710268cd5d5abd8ce3abd69a7f352c3f78a72e
gcmartin <gscalica@gmail.com>
2020-04-21 21:28:25 -0700

    Add code for DELETE request, add comments, and clean up code

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+// generate random ids
+const uuid = require("uuid/v4");
+
 // Set up the Express app
 const app = express();
 const PORT = 3001;
@@ -42,12 +45,9 @@ app.get("/api/notes", function(req, res){
             // respond to user with the json data
             res.json(noteData);
     });
-
-    
-    // res.sendFile(__dirname + "/db/db.json");
 });
 
-// // (POST) API Notes route
+// (POST) API Notes route
 app.post("/api/notes", function(req, res){
     // read the db.json file
     fs.readFile("./db/db.json", "utf8", function(err, data){
@@ -57,15 +57,15 @@ app.post("/api/notes", function(req, res){
             // parse the data and store to variable
             let noteData = JSON.parse(data);
 
+            // give random id to note when it's saved
+            req.body.id = uuid();
+
             // store the newNote from the POST request to a new variable
             let newNote = req.body;
             console.log(newNote);
 
             // push the newNote into noteData (it's an array)
             noteData.push(newNote); 
-
-            // see what the data is
-            console.log(JSON.stringify(noteData));
 
             // write file
             fs.writeFile("./db/db.json", JSON.stringify(noteData), function(err){
@@ -77,27 +77,60 @@ app.post("/api/notes", function(req, res){
 
             // respond to the user with the new note
             res.json(noteData);
-
-        
     });
-    // X save the new note to a variable
-    // X push the new note to db.json
-    // X respond to client with the new note
-
 });
 
 
-// Delete request will go here
+// (DELETE) API Notes Route
+app.delete("/api/notes/:id", function(req, res){
+    // Get the :id from the front end
+    let urlid = req.params.id;
 
+    // read the json file
+    fs.readFile("./db/db.json", "utf8", function(err, data){
+        if(err){
+            console.log(err);
+        }
+            // if no error obtained, run code below
 
+            // parse the current notes data
+            const currentNotes = JSON.parse(data);
 
+            // loop through the currentNotes array and find the matching id
+            for(let i = 0; i < currentNotes.length; i++){
+
+                // if the currentNotes id equals the front end id
+                if(currentNotes[i].id === urlid){
+                    // getting the index of an object inside an array:  
+                    // https://stackoverflow.com/questions/15997879/get-the-index-of-the-object-inside-an-array-matching-a-condition
+                    let index = currentNotes.findIndex(x => x.id === urlid);
+
+                    // let indexOfNote = currentNotes.indexOf(urlid);
+                    console.log("this note will be deleted");
+
+                    // remove the object with the matching id
+                    currentNotes.splice(index, 1);
+
+                    // rewrite json file with the updated notes
+                    fs.writeFile("./db/db.json", JSON.stringify(currentNotes), function(err){
+                        if(err){
+                            return console.log(err);
+                        }
+                            console.log("success!");
+                    });
+
+                    // respond to the user with the new notes
+                    res.json(currentNotes);
+
+                    console.log(currentNotes);
+                } 
+            }
+    });
+});
 
 // ============================
-
-
-
 
 // Start server for listening
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
-})
+});
